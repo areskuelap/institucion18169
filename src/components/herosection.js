9bd33ef806802463638cdb517e277a5cf7636eee
areskuelap <83573119+areskuelap@users.noreply.github.com>
2023-02-17 20:00:03 -0500

    Update herosection.js

--- src/components/herosection.js
+++ src/components/herosection.js
@@ -4,16 +4,20 @@ const HeroSection = () => {
   return (
     <section className="bg-gray-100 py-12">
       <div className="container py-8 mx-auto sm:flex justify-between items-center">
-        <div className="w-11/12 sm:w-3/5">
-          <h2 className="text-xl sm:text-3xl text-gray-600 font-semibold mb-4 font-mono">
+        <div className="w-full sm:w-3/5">
+      <div className="flex justify-center">
+      <div className="w-11/12">
+              <h2 className="text-xl sm:text-3xl text-gray-600 font-semibold mb-4 font-mono">
             Institución Educativa 18169
           </h2>
-          <p className="text-xl mb-6 px-16">
+              <p className="text-xl mb-6 px-16">
           La Institución educativa Escuela 18169 se ubica en el distrito de Ocumal, localidad de Congón, provincia de Luya, tal IE compete a la UGEL LUYA que inspecciona el servicio educativo, que pertenece a la Dirección regional de educación DRE AMAZONAS.
           </p>
           <a href="https://joseluisaristatejada.blogspot.com/2019/02/instrumentos-de-gestion-escolar.html" target="_blank"><button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
             Ver más
           </button></a>
+    </div>
+      </div>
         </div>
         <div className="w-full pt-10 sm:w-2/5 flex justify-center">
         <img
