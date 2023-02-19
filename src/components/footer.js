183a6b6a6b3d2f133acb519d48cd1ac976903be3
areskuelap <83573119+areskuelap@users.noreply.github.com>
2023-02-19 18:29:29 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
           <h1 className="text-lg font-bold">Amazonas</h1>
         </div>
         <div className="flex space-x-6">
-          <a href="#">
+          <a href="https://web.facebook.com/groups/1678466822249266 target="_blank"">
             <FaFacebook className="w-6 h-6" />
           </a>
-          <a href="#">
+          <a href="https://web.facebook.com/groups/1678466822249266 target="_blank"">
             <FaTwitter className="w-6 h-6" />
           </a>
-          <a href="#">
+          <a href="https://web.facebook.com/groups/1678466822249266" target="_blank">
             <FaInstagram className="w-6 h-6" />
           </a>
         </div>
